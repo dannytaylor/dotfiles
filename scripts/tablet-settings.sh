a61cd849c284edc253633ce8c6b438399d13a192
dt <dhtxhg@gmail.com>
2017-01-17 18:16:49 -0800

    added expresskeys settings and left handed settings for tablet

---
+++ scripts/tablet-settings.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+xsetwacom --set 'HUION H610 Pen stylus' rotate half
+xsetwacom --set 'HUION H610 Pad pad' Button 1 "key +ctrl +z -z -ctrl" 
+xsetwacom --set 'HUION H610 Pad pad' Button 2 "key e"
+xsetwacom --set 'HUION H610 Pad pad' Button 3 "key +ctrl"
+xsetwacom --set 'HUION H610 Pad pad' Button 8 "key +ctrl +minus"    
+xsetwacom --set 'HUION H610 Pad pad' Button 9 "key +ctrl +shift +plus"
+xsetwacom --set 'HUION H610 Pad pad' Button 10 "key ]"
+xsetwacom --set 'HUION H610 Pad pad' Button 11 "key ["
+xsetwacom --set 'HUION H610 Pad pad' Button 12 "key +space"
+
